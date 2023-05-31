84f24352231537b70354f46051cd12ed977f6bc7
Victor_Vaz <63057845+VictorVaz-Dev@users.noreply.github.com>
2023-05-31 00:06:16 -0300

    adicionado javascript
    adicionei javascript e e trocado a cor do site para azul e laranja, foi adicionado os botões também

---
+++ SITES-SECUNDARIOS/BOOTSTRAP/JS/boot.js
@@ -0,0 +1,9 @@
+let motoazul = document.getElementById('moto-azul');
+let motolaranja = document.getElementById('moto-laranja');
+
+window.addEventListener('scroll', () => {
+    let value = window.scrollY;
+
+    motoazul.style.left = value * -1.5 + "px";
+    motolaranja.style.left = value * 1.5 + "px";
+});
