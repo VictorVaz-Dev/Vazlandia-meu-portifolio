45155c1ad387cedd9082522c3d6e4d6176eb5714
Victor_Vaz <63057845+VictorVaz-Dev@users.noreply.github.com>
2024-12-17 09:28:39 -0300

    apagando todo o projeto
    apagando todo o projeto para utilizar tecnologias mais atuais

--- SITE-PRINCIPAL/JS/principal.js
+++
@@ -1,9 +0,0 @@
-let lua = document.getElementById('lua');
-let estrelas = document.getElementById('estrelas');
-
-window.addEventListener('scroll', () => {
-    let value = window.scrollY;
-
-    lua.style.top = value * 1.05 + 'px';
-    estrelas.style.left = value * 0.25 + 'px';
-});
--- SITES-SECUNDARIOS/BOOTSTRAP/JS/boot.js
+++
@@ -1,9 +0,0 @@
-let motoazul = document.getElementById('motoazul');
-let motolaranja = document.getElementById('motolaranja');
-
-window.addEventListener('scroll', () => {
-    let value = window.scrollY;
-
-    motoazul.style.left = value * 1.5 + 'px';
-    motolaranja.style.left = value * -1.5 + 'px';
-});
